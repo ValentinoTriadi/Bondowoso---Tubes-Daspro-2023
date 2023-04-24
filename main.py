25722cbdfcb89a78b242371b9f0e42811dc7518f
ValentinoTriadi <valent122016@gmail.com>
2023-04-24 17:26:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,88 +1,93 @@
 import Main_Function, time, load_only
 
+'''---------------------------------------------------------- Starting Point ----------------------------------------------------------'''
 if __name__ == "__main__":
-    load_only.load()
-    while load_only.start:
-        menu = input(">>> ")
+
+    load_only.load() # Melakukan load folder
+
+    while load_only.start: # Saat status start = True
         import Visual
-        if menu == "login" and not Main_Function.logins:
+        
+        menu = input(">>> ") # Meminta input user
+        if menu == "login" and not Main_Function.logins: # Saat user memasukan command login dan user belum login  
             Main_Function.login()
 
-        elif menu == "login" and Main_Function.logins:
-            Visual.render_screen(["Login gagal!",f"Anda telah login dengan username {Main_Function.user}, silahkan lakukan “logout” sebelum melakukan login kembali."],2)
+        elif menu == "login" and Main_Function.logins: # Saat user memasukan command login namun user sudah login
+            Visual.render_screen(["Login gagal!",f"Anda telah login dengan username {Main_Function.user}, silahkan lakukan “logout” sebelum melakukan login kembali."],2) # Pesan Kesalahan
             time.sleep(2)
 
-        elif menu == "logout" and Main_Function.logins:
+        elif menu == "logout" and Main_Function.logins: # Saat user memasukan command logout dan user sudah login
             Main_Function.logout()
-            Visual.printascii("home",None)
+            Visual.printascii("home",None) # Tampilan Home Awal
 
-        elif menu == "logout" and not Main_Function.logins:
+        elif menu == "logout" and not Main_Function.logins: # Saat user memasukan command logout namun user belum login
             Visual.render_screen(["Logout gagal!",f"Anda belum login, silahkan login terlebih dahulu sebelum melakukan logout"],2)
             time.sleep(2)
-            Visual.printascii("home",None)
+            Visual.printascii("home",None) # Tampilan Home Awal 
 
-        elif menu == "summonjin" and Main_Function.access == "bandung_bondowoso":
+        elif menu == "summonjin" and Main_Function.access == "bandung_bondowoso": # Saat user memasukan command summonjin dan memiliki akses Bondowoso
             Main_Function.summonJin()
-            Visual.printascii(Main_Function.access,Main_Function.access)
+            Visual.printascii(Main_Function.access,Main_Function.access) # Tampilan Karakter sesuai dengan role
 
-        elif menu == "hapusjin" and Main_Function.access == "bandung_bondowoso":
+        elif menu == "hapusjin" and Main_Function.access == "bandung_bondowoso": # Saat user memasukan command hapusjin dan memiliki akses Bondowoso
             Main_Function.hapusJin()
-            Visual.printascii(Main_Function.access,Main_Function.access)
+            Visual.printascii(Main_Function.access,Main_Function.access) # Tampilan Karakter sesuai dengan role
 
-        elif menu == "ubahjin" and Main_Function.access == "bandung_bondowoso":
+        elif menu == "ubahjin" and Main_Function.access == "bandung_bondowoso": # Saat user memasukan command ubahjin dan memiliki akses Bondowoso
             Main_Function.ubahJin()
-            Visual.printascii(Main_Function.access,Main_Function.access)
+            Visual.printascii(Main_Function.access,Main_Function.access) # Tampilan Karakter sesuai dengan role
 
-        elif menu == "bangun" and Main_Function.access == "Pembangun":
+        elif menu == "bangun" and Main_Function.access == "Pembangun": # Saat user memasukan command bangun dan memiliki akses Pembangun
             Main_Function.bangun(Main_Function.user)
-            Visual.printascii(Main_Function.access,Main_Function.access)
+            Visual.printascii(Main_Function.access,Main_Function.access) # Tampilan Karakter sesuai dengan role
 
-        elif menu == "kumpul" and Main_Function.access == "Pengumpul":
+        elif menu == "kumpul" and Main_Function.access == "Pengumpul": # Saat user memasukan command kumpul dan memiliki akses Pengumpul
             Main_Function.kumpul(False)
-            Visual.printascii(Main_Function.access,Main_Function.access)
+            Visual.printascii(Main_Function.access,Main_Function.access) # Tampilan Karakter sesuai dengan role
 
-        elif menu == "batchkumpul" and Main_Function.access == "bandung_bondowoso":
+        elif menu == "batchkumpul" and Main_Function.access == "bandung_bondowoso": # Saat user memasukan command batchkumpul dan memiliki akses Bondowoso
             Main_Function.batchkumpul()
-            Visual.printascii(Main_Function.access,Main_Function.access)
+            Visual.printascii(Main_Function.access,Main_Function.access) # Tampilan Karakter sesuai dengan role
 
-        elif menu == "batchbangun" and Main_Function.access == "bandung_bondowoso":
+        elif menu == "batchbangun" and Main_Function.access == "bandung_bondowoso": # Saat user memasukan command batchbangun dan memiliki akses Bondowoso
             Main_Function.batchbangun()
-            Visual.printascii(Main_Function.access,Main_Function.access)
+            Visual.printascii(Main_Function.access,Main_Function.access) # Tampilan Karakter sesuai dengan role
 
-        elif menu == "laporanjin" and Main_Function.access == "bandung_bondowoso":
+        elif menu == "laporanjin" and Main_Function.access == "bandung_bondowoso": # Saat user memasukan command laporanjin dan memiliki akses Bondowoso
             Main_Function.laporanjin()
-            Visual.printascii(Main_Function.access,Main_Function.access)
+            Visual.printascii(Main_Function.access,Main_Function.access) # Tampilan Karakter sesuai dengan role
 
-        elif menu == "laporancandi" and Main_Function.access == "bandung_bondowoso":
+        elif menu == "laporancandi" and Main_Function.access == "bandung_bondowoso": # Saat user memasukan command laporancandi dan memiliki akses Bondowoso
             Main_Function.laporancandi()
-            Visual.printascii(Main_Function.access,Main_Function.access)
+            Visual.printascii(Main_Function.access,Main_Function.access) # Tampilan Karakter sesuai dengan role
             
-        elif menu == "hancurkancandi" and Main_Function.access == "roro_jonggrang":
+        elif menu == "hancurkancandi" and Main_Function.access == "roro_jonggrang": # Saat user memasukan command hancurkancandi dan memiliki akses Roro
             Main_Function.hancurkancandi()
-            Visual.printascii(Main_Function.access,Main_Function.access)
+            Visual.printascii(Main_Function.access,Main_Function.access) # Tampilan Karakter sesuai dengan role
 
-        elif menu == "ayamberkokok" and Main_Function.access == "roro_jonggrang":
+        elif menu == "ayamberkokok" and Main_Function.access == "roro_jonggrang": # Saat user memasukan command ayamberkokok dan memiliki akses Roro
             Main_Function.ayamberkokok()
             load_only.start = False
 
-        elif menu == "exit":
+        elif menu == "exit": # Saat user memasukan command exit
             Main_Function.keluar()
             load_only.start = False
 
-        elif menu == "help":
+        elif menu == "help": # Saat user memasukan command help
             Main_Function.help()
             
-        elif menu == "save":
+        elif menu == "save": # Saat user memasukan command save
             Main_Function.save()
-            Visual.printascii(Main_Function.access,Main_Function.access)
+            Visual.printascii(Main_Function.access,Main_Function.access) # Tampilan Karakter sesuai dengan role 
 
-        elif Main_Function.logins:
-            Visual.render_screen(["Access Denied"],1)
+        elif Main_Function.logins: # Validasi saat user sudah login namun salah menginputkan command yang tidak sesuai dengan akses user
+            Visual.render_screen(["ACCESS DENIED",'Command yang dimasukan salah!'],2) # Pesan Kesalahan
             time.sleep(1.5)
-            Visual.printascii(Main_Function.access,Main_Function.access)
+            Visual.printascii(Main_Function.access,Main_Function.access) # Tampilan Karakter sesuai dengan role 
 
-        elif not Main_Function.logins:
-            import Visual
-            Visual.render_screen(["Access Denied","Anda Belum Login!"],2)
+        elif not Main_Function.logins: # Validasi saat user belum login dan menginputkan command yang memerlukan akses
+            Visual.render_screen(["ACCESS DENIED","Anda Belum Login!"],2) # Pesan Kesalahan
             time.sleep(1.5)
-            Visual.printascii("home",Main_Function.access)
+            Visual.printascii("home",None) # Tampilan Home Awal
+
+'''---------------------------------------------------------- Starting Point ----------------------------------------------------------'''
